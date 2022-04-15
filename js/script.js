d530d1d2c3015c00f53a7883d7d104ac5624d552
LaroPam <79053728+LaroPam@users.noreply.github.com>
2022-04-15 20:41:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-$(".header__burger").on("click", function() {
-    $(this).toggleClass("active");
+$(".header__burger, .nav__item").on("click", function() {
+    $(".header__burger").toggleClass("active");
     $(".nav").toggleClass("visible");
     $("body").toggleClass("hidden");
 })
