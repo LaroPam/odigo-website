f2ee7799bf41de67b1f33f70dd65f53a8b8098b5
LaroPam <79053728+LaroPam@users.noreply.github.com>
2022-04-16 13:40:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(".header__burger, .nav__item").on("click", function() {
+$(".header__burger").on("click", function() {
     $(".header__burger").toggleClass("active");
     $(".nav").toggleClass("visible");
     $("body").toggleClass("hidden");
